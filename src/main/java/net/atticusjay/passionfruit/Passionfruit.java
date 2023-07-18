5637e51b79d51b07186dd2930a9dbe07e59e75a6
AtticusJay <atticusjayhelvig@gmail.com>
2023-07-17 21:28:12 -0700

    Initial commit

---
+++ src/main/java/net/atticusjay/passionfruit/Passionfruit.java
@@ -0,0 +1,15 @@
+package net.atticusjay.passionfruit;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Passionfruit implements ModInitializer {
+	public static final String MOD_ID = "passionfruit";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/atticusjay/passionfruit/PassionfruitClient.java
@@ -0,0 +1,10 @@
+package net.atticusjay.passionfruit;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PassionfruitClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
